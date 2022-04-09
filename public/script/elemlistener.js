2e3e3d878137d043362c71225077f07567c5327d
Alex Westergaard <aw.freelancer@gmail.com>
2022-04-09 16:17:47 +0200

    Add first draft og fully functional server

---
+++ public/script/elemlistener.js
@@ -0,0 +1,342 @@
+const clickListener = function () {
+    this.bindTeam = ''
+    this.bindItem = ''
+    this.bindType = ''
+
+    this.hasSelectables = false
+    this.hasChamps = false
+    this.hasSpells = false
+
+    this.getChamp = '/img/champ/%.png'
+    this.getChampBanner = '/img/banner/%.png'
+    this.getChampSpell = '/img/spell/%.png'
+
+    this.search = null
+}
+
+clickListener.prototype.clickChamp = function (elem = null) {
+    if (
+        this.bindType === null
+        || this.bindTeam === null
+        || this.bindItem === null
+        || this.bindType != 'champ'
+        || elem === null
+    ) {
+        alert('Something went wrong, tried to select champ in the wrong state.')
+        this.clickSelect()
+        return
+    }
+
+    const setValue = $(elem).attr('data-champ')
+
+    axios({
+        method: 'POST',
+        url: `/cache`,
+        data: {
+            team: this.bindTeam,
+            slot: this.bindItem,
+            value: setValue
+        }
+    }).then(() => {
+        const refPath = `/cache/${this.bindTeam}/${this.bindItem}.png?${Date.now()}`
+        const refElem = $(`[data-team="${this.bindTeam}"][data-item="${this.bindItem}"]`)
+        setTimeout(() => {
+            refElem.attr('src', refPath)
+        }, 100);
+    }).catch(err => {
+        console.error(err)
+    }).finally(() => {
+        this.clickSelect()
+    })
+}
+
+clickListener.prototype.clickSpell = function (elem = null) {
+    if (
+        this.bindType === null
+        || this.bindTeam === null
+        || this.bindItem === null
+        || this.bindType != 'spell'
+        || elem === null
+    ) {
+        alert('Something went wrong, tried to select spell in the wrong state.')
+        this.clickSelect()
+        return
+    }
+
+    const setValue = $(elem).attr('data-spell')
+
+    axios({
+        method: 'POST',
+        url: `/cache`,
+        data: {
+            team: this.bindTeam,
+            slot: this.bindItem,
+            value: setValue
+        }
+    }).then(() => {
+        const refElem = $(`[data-team="${this.bindTeam}"][data-item="${this.bindItem}"]`)
+        const refPath = `/cache/${this.bindTeam}/${this.bindItem}.png?${Date.now()}`
+        setTimeout(() => {
+            refElem.attr('src', refPath)
+        }, 100);
+    }).catch(err => {
+        console.error(err)
+    }).finally(() => {
+        this.clickSelect()
+    })
+}
+
+clickListener.prototype.clickSelect = function (elem = null) {
+    const isSelected = elem?.hasClass('is-select')
+
+    this.bindType = null
+    this.bindTeam = null
+    this.bindItem = null
+
+    $('[data-select]').removeClass('is-select')
+
+    if (!isSelected && elem !== null) {
+        this.bindTeam = elem.attr('data-team')
+        this.bindItem = elem.attr('data-item')
+        elem.addClass('is-select')
+        this.bindType = elem.attr('data-select')
+    }
+
+    if (this.bindType == 'champ') {
+        $('.selection .champs').slideDown(300)
+        $('.selection .spells').hide()
+        $('html, body').animate({
+            scrollTop: $("section.selection").offset().top
+        }, 336);
+        $('.selection .searchfor input').trigger('focus')
+    } else if (this.bindType == 'spell') {
+        $('.selection .spells').slideDown(300)
+        $('.selection .champs').hide()
+        $('html, body').animate({
+            scrollTop: $("section.selection").offset().top
+        }, 336);
+        $('.selection .searchfor input').trigger('focus')
+    } else {
+        $('.selection .spells').hide()
+        $('.selection .champs').hide()
+        $('.selection .searchfor input').val('')
+        $(`.selection .champs .champ, .selection .spells .spell`).fadeIn(133)
+    }
+}
+
+clickListener.prototype.loadSelectables = function () {
+    if (this.hasSelectables) return this
+    const self = this
+
+    axios({
+        method: 'GET',
+        url: `/cache?${Date.now()}`
+    }).then(res => {
+        if (!res.data || typeof res.data != 'object') return
+        self.hasSelectables = true
+
+        for (const [team, teamData] of Object.entries(res.data)) {
+            for (const [item, value] of Object.entries(teamData)) {
+                $(`[data-team="${team}"][data-item="${item}"]`).each((idx, elem) => {
+                    if (elem.tagName == 'IMG') {
+                        elem.src = `/cache/${team}/${item}.png`
+                    }
+
+                    elem.classList.add('clickable')
+                })
+            }
+        }
+
+        $('[data-select]').on('click', function () {
+            self.clickSelect($(this))
+        })
+    })
+
+    axios({
+        method: 'GET',
+        url: `/lookup/names?${Date.now()}`
+    }).then(res => {
+        if (!res.data || typeof res.data != 'object') return
+        self.hasSelectables = true
+
+        for (const [field, value] of Object.entries(res.data)) {
+            $(`[data-name="${field}"]`).each((idx, elem) => {
+                if (elem.tagName == 'INPUT') {
+                    elem.value = value
+                } else if (elem.tagName != 'INPUT') {
+                    elem.innerText = value
+                }
+
+                elem.classList.add('clickable')
+            })
+        }
+    })
+
+    return this
+}
+
+clickListener.prototype.loadChamps = function () {
+    if (this.hasChamps) return this
+    const self = this
+
+    axios({
+        method: 'GET',
+        url: `/lookup/champs?${Date.now()}`
+    }).then(res => {
+        if (!res.data || typeof res.data != 'object') return
+        self.hasChamps = true
+
+        Object.values(res.data).forEach((champ, champNum) => {
+            $('.selection .champs').append(
+                `<div class="champ item clickable" data-champ="${champ}" data-search="${champ.toLowerCase()}">
+                    <img class="icon" src="${this.getChamp.replace('%', champ)}" alt="${champ}" loading="lazy">
+                    <span class="id">${champNum + 1}</span>
+                    <span class="name">${champ}</span>
+                </div>`
+            )
+        })
+
+        $('.selection .champs').prepend(
+            `<div class="champ item clickable" data-champ="" data-search="reset">
+                <img class="icon" src="/default/champion.png" alt="Reset" loading="lazy">
+                <span class="id">0</span>
+                <span class="name">Reset</span>
+            </div>`
+        )
+
+        $('[data-champ]').on('click', function () {
+            self.clickChamp($(this))
+        })
+    })
+
+    return this
+}
+
+clickListener.prototype.loadSpells = function () {
+    if (this.hasSpells) return this
+    const self = this
+
+    axios({
+        method: 'GET',
+        url: `/lookup/spells?${Date.now()}`
+    }).then(res => {
+        if (!res.data || typeof res.data != 'object') return
+        self.hasSpells = true
+
+        Object.values(res.data).forEach((spell, spellNum) => {
+            spellName = spell.replace('Summoner', '')
+            $('.selection .spells').append(
+                `<div class="spell item clickable" data-spell="${spell}" data-search="${spellName.toLowerCase()}">
+                    <img class="icon" src="${this.getChampSpell.replace('%', spell)}" alt="${spellName}" loading="lazy">
+                    <span class="id">${spellNum + 1}</span>
+                    <span class="name">${spellName}</span>
+                </div>`
+            )
+        })
+
+        $('.selection .spells').prepend(
+            `<div class="spell item clickable" data-spell="" data-search="reset">
+                <img class="icon" src="/default/spell.png" alt="reset" loading="lazy">
+                <span class="id">0</span>
+                <span class="name">Reset</span>
+            </div>`
+        )
+
+        $('[data-spell]').on('click', function () {
+            self.clickSpell($(this))
+        })
+    })
+
+    return this
+}
+
+clickListener.prototype.bindClickables = function () {
+    $('button[data-swap]').on('click', function () {
+        $('button[data-swap]').attr('disabled', true)
+        axios({
+            method: 'POST',
+            url: '/action/swap'
+        }).then(res => {
+            setTimeout(() => {
+                $('button[data-swap]').removeAttr('disabled')
+            }, 400)
+            if (!res?.data || typeof res?.data != 'object') return
+
+            for (const [field, value] of Object.entries(res.data)) {
+                $(`input[data-name="${field}"]`).each((idx, elem) => {
+                    elem.value = value
+                })
+            }
+        })
+    })
+
+    $('input[data-name]').on('click', function (event) {
+        const elem = event.currentTarget
+        const id = elem.getAttribute('data-name')
+        const value = elem.value.toString()
+        const setValue = prompt(`New name for ${id}?`)
+
+        if (!setValue || setValue == value) return
+
+        axios({
+            method: 'POST',
+            url: `/action/name`,
+            data: {
+                slot: id,
+                value: setValue
+            }
+        }).then(res => {
+            elem.value = res.data?.name
+        })
+    })
+
+    $('button[data-reset]').on('click', function () {
+        if (!confirm("Sure you want to reset all selected images?")) return
+
+        axios({
+            method: 'POST',
+            url: `/action/reset`,
+        }).then(res => {
+            if (!res?.data || typeof res.data != 'object') return
+
+            for (const [team, teamData] of Object.entries(res.data)) {
+                for (const [item, value] of Object.entries(teamData)) {
+                    $(`[data-team="${team}"][data-item="${item}"]`).each((idx, elem) => {
+                        if (elem.tagName == 'IMG') {
+                            elem.src = `/cache/${team}/${item}.png?${Date.now()}`
+                        } else if (elem.tagName == 'INPUT') {
+                            elem.value = value
+                        }
+                    })
+                }
+            }
+        }).catch(err => {
+            const res = err.response
+            alert(res?.data?.message || 'Failed to reset')
+        })
+    })
+    return this
+}
+
+clickListener.prototype.bindSearch = function () {
+    const self = this
+
+    $('.selection .searchfor input').on('input', (e) => {
+        const me = $(e.currentTarget)
+        clearTimeout(self.search)
+        setTimeout(() => {
+            const val = me.val().toLowerCase()
+            if (val.length > 0) {
+                $(`.selection .champs .champ:not([data-search*="${val}"]), .selection .spells .spell:not([data-search*="${val}"])`).fadeOut(222)
+                $(`.selection .champs .champ[data-search*="${val}"], .selection .spells .spell[data-search*="${val}"]`).fadeIn(133)
+            } else {
+                $(`.selection .champs .champ, .selection .spells .spell`).fadeIn(133)
+            }
+            $('html, body').animate({
+                scrollTop: $("section.selection").offset().top
+            }, 336);
+        }, 336)
+    })
+
+    return this
+}
---
+++ server.js
@@ -0,0 +1,228 @@
+require('dotenv').config()
+const os = require('os')
+const fs = require('fs')
+const path = require('path')
+const axios = require('axios')
+const express = require('express')
+const crypto = require("crypto")
+const caching = require('./core/caching.js')
+
+const app = express()
+const serverLocal = '127.0.0.1'
+const serverIp = process.env?.SERVER_PUBLIC == 1 ? '0.0.0.0' : serverLocal
+const serverPort = Number(process.env?.SERVER_PORT || 3000)
+const validateServerPath = crypto.randomBytes(64).toString('hex')
+const validateServer = crypto.randomBytes(32).toString('hex')
+
+app.use(express.json())
+app.use('/script', express.static('node_modules/axios/dist'))
+app.use('/script', express.static('node_modules/jquery/dist'))
+app.use('/cache', express.static('cache', {
+    setHeaders: function (res, path) {
+        res.set({
+            'Cache-Control': 'private, max-age=0, must-revalidate',
+        })
+    }
+}))
+app.use('/live', express.static('cache', {
+    setHeaders: function (res, path) {
+        res.set({
+            'Refresh': 2,
+            'Cache-Control': 'private, max-age=0, must-revalidate',
+        })
+    }
+}))
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    const loadPath = path.resolve('core/www/dashboard.html')
+
+    let setHeaders = {
+        'Cache-Control': 'no-cache, must-revalidate',
+        'Content-Type': 'text/html',
+        'ETag': false
+    }
+
+    res.set(setHeaders).sendFile(loadPath)
+})
+
+app.get('/cache', (req, res) => {
+    let setHeaders = {
+        'Cache-Control': 'no-cache, must-revalidate',
+        'Content-Type': 'application/json',
+        'ETag': false
+    }
+
+    res.set(setHeaders).send(caching.getState())
+})
+
+app.post('/cache', (req, res) => {
+    let setHeaders = {
+        'Cache-Control': 'no-cache, must-revalidate',
+        'Content-Type': 'application/json',
+        'ETag': false
+    }
+    const team = req.body?.team
+    const slot = req.body?.slot
+    const value = req.body?.value
+
+    caching.push(team, slot, value).then(() => {
+        res.set(setHeaders).send()
+    }).catch(() => {
+        res.status(400).set(setHeaders).send()
+    })
+})
+
+app.get('/lookup/champs', (req, res) => {
+    const loadPath = path.resolve('core/data/champs.json')
+
+    let setHeaders = {
+        'Cache-Control': 'no-cache, must-revalidate',
+        'Content-Type': 'application/json',
+        'ETag': false
+    }
+
+    res.set(setHeaders).sendFile(loadPath)
+})
+
+app.get('/lookup/spells', (req, res) => {
+    const loadPath = path.resolve('core/data/spells.json')
+
+    let setHeaders = {
+        'Cache-Control': 'no-cache, must-revalidate',
+        'Content-Type': 'application/json',
+        'ETag': false
+    }
+
+    res.set(setHeaders).sendFile(loadPath)
+})
+
+app.get('/lookup/names', (req, res) => {
+    const loadNameRed = path.resolve('cache/red/name.txt')
+    const loadNameBlue = path.resolve('cache/blue/name.txt')
+    const loadNameMatch = path.resolve('cache/match.txt')
+
+    const nameBlue = fs.readFileSync(loadNameBlue, 'utf8')
+    const nameRed = fs.readFileSync(loadNameRed, 'utf8')
+    const nameMatch = fs.readFileSync(loadNameMatch, 'utf8')
+
+    let setHeaders = {
+        'Cache-Control': 'no-cache, must-revalidate',
+        'Content-Type': 'application/json',
+        'ETag': false
+    }
+
+    res.set(setHeaders).send({
+        red: nameRed,
+        blue: nameBlue,
+        match: nameMatch
+    })
+})
+
+app.post('/action/name', (req, res) => {
+    const srcRed = path.resolve('cache/red/name.txt')
+    const srcBlue = path.resolve('cache/blue/name.txt')
+    const srcMatch = path.resolve('cache/match.txt')
+
+    const slot = req.body?.slot
+    const value = req.body?.value
+    const src = slot == 'blue' ? srcBlue : (slot == 'red' ? srcRed : (slot == 'match' ? srcMatch : null))
+
+    if (!slot || !src) {
+        return res.status(400).send({ name: value })
+    }
+
+    fs.writeFileSync(src, value, err => {
+        if (!err) return
+        console.error(err)
+    })
+
+    let setHeaders = {
+        'Cache-Control': 'no-cache, must-revalidate',
+        'Content-Type': 'application/json',
+        'ETag': false
+    }
+
+    res.set(setHeaders).send({
+        name: value
+    })
+})
+
+app.post('/action/reset', (req, res) => {
+    caching.build(true).then(callback => {
+        if (callback.errors.length > 0) {
+            return res.status(400).send()
+        }
+
+        let setHeaders = {
+            'Cache-Control': 'no-cache, must-revalidate',
+            'Content-Type': 'application/json',
+            'ETag': false
+        }
+
+        res.set(setHeaders).send(caching.getState())
+    })
+})
+
+app.post('/action/swap', (req, res) => {
+    const loadNameRed = path.resolve('cache/red/name.txt')
+    const loadNameBlue = path.resolve('cache/blue/name.txt')
+
+    const nameBlue = fs.readFileSync(loadNameBlue, 'utf8')
+    const nameRed = fs.readFileSync(loadNameRed, 'utf8')
+
+    fs.writeFileSync(loadNameRed, nameBlue, err => {
+        if (!err) return
+        console.error(err)
+    })
+    fs.writeFileSync(loadNameBlue, nameRed, err => {
+        if (!err) return
+        console.error(err)
+    })
+
+    let setHeaders = {
+        'Cache-Control': 'no-cache, must-revalidate',
+        'Content-Type': 'application/json',
+        'ETag': false
+    }
+
+    res.set(setHeaders).send({
+        red: nameBlue,
+        blue: nameRed
+    })
+})
+
+app.post(`/${validateServerPath}`, (req, res) => {
+    res.send(validateServer)
+})
+
+caching.build().then(() => {
+    console.log("Build the cache from core/data/cache.json")
+    app.listen(serverPort, serverIp, () => {
+        const host = `http://${serverLocal}:${serverPort}`
+        console.log(`Local host: ${host}`)
+
+        const getIpTable = os.networkInterfaces()
+        for (const [key, packet] of Object.entries(getIpTable)) {
+            packet.forEach(row => {
+                const ip = row.family == 'IPv6' ? `[${row.address}]` : row.address
+                if (ip == serverLocal) return
+
+                const host = `http://${ip}:${serverPort}`
+                const self = `${host}/${validateServerPath}`
+
+                axios({
+                    method: 'POST',
+                    url: self
+                }).then(res => {
+                    if (res.data == validateServer) {
+                        console.log(`Public host: ${host}`)
+                    }
+                }).catch(err => null)
+            })
+        }
+    })
+}).catch(err => {
+    console.error(err)
+    console.log('NOTICE: Server is public')
+})
