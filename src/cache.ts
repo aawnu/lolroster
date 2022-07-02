f025a2c1d2fbd6e9f8b9a8da86418c0c26ae9151
Alex Westergaard <aw.freelancer@gmail.com>
2022-07-02 16:09:07 +0200

    Fix: Update interfaces to types
    This seems to fix the issue at hand inside the cache at least

--- src/cache.ts
+++ src/cache.ts
@@ -4,29 +4,23 @@ import fs from 'fs'
 import path from 'path'
 import Jimp from 'jimp'
 
-enum mediaTypes {
+export enum cacheTypesMedia {
     'png' = '.png',
     'jpg' = '.jpg',
     'jpeg' = '.jpg',
     'gif' = '.gif',
     'bmp' = '.bmp',
+}
+
+export enum cacheTypesText {
     'txt' = '.txt',
 }
 
-export interface LolTeamLayout {
-    ban1: string, champ1: string, champ1spell1: string, champ1spell2: string,
-    ban2: string, champ2: string, champ2spell1: string, champ2spell2: string,
-    ban3: string, champ3: string, champ3spell1: string, champ3spell2: string,
-    ban4: string, champ4: string, champ4spell1: string, champ4spell2: string,
-    ban5: string, champ5: string, champ5spell1: string, champ5spell2: string,
-}
+export type LolTeamLayout = { [key: string]: string }
 
-export interface LolTeams {
-    red: LolTeamLayout,
-    blue: LolTeamLayout
-}
+export type LolTeams = { [key: string]: LolTeamLayout }
 
-export let teams: LolTeams = {
+export let teamsStructure: LolTeams = {
     red: {
         ban1: '', champ1: '', champ1spell1: '', champ1spell2: '',
         ban2: '', champ2: '', champ2spell1: '', champ2spell2: '',
@@ -42,6 +36,8 @@ export let teams: LolTeams = {
         ban5: '', champ5: '', champ5spell1: '', champ5spell2: '',
     }
 }
+
+export let teams: LolTeams = teamsStructure
 
 const srcCache = path.resolve('cache')
 const srcData = path.resolve('core/data')
@@ -71,7 +67,7 @@ const lookupFile = function (iPath: string, iName: string): string | void {
     }
 }
 
-const saveCacheImage = function (loadPath: string, savePath: string): void {
+const saveCacheImage = (loadPath: string, savePath: string): void => {
     Jimp.read(loadPath, (err, img) => {
         if (err) {
             console.error(err)
@@ -81,101 +77,109 @@ const saveCacheImage = function (loadPath: string, savePath: string): void {
     })
 }
 
-const saveCacheText = function (savePath: string, saveBody: string) {
+const saveCacheText = (savePath: string, saveBody: string) => {
     fs.writeFile(savePath, saveBody, err => {
         if (err) console.error(err)
     })
 }
 
-export const push = (team: string, slot: string, slotItem: string) => new Promise<boolean>((resolve, reject) => {
-    if (typeof teams?.[team]?.[slot] != 'string') {
-        reject(false)
-    }
-
-    teams[team][slot] = slotItem
-
-    const slotType: string[] | null = slot.match(/^(ban|champ)[1-5](spell)?[1-2]?/)
-    const saveSpot: string = path.resolve(srcCache, team, slot + savePng)
-
-    let lookup = {
-        champ: path.resolve('' + env('CHAMP_ICON_LOOKUP')),
-        banner: path.resolve('' + env('CHAMP_BANNER_LOOKUP')),
-        spell: path.resolve('' + ('SPELL_ICON_LOOKUP'))
-    }
-
-    let saveText: boolean = false
-    let image: string = ''
-
-    if (slotType?.[1] == 'champ' && slotType?.[2] == 'spell') {
-        image = lookupFile(
-            lookup.spell,
-            slotItem
-        ) || defaultSpell
-    } else if (!slotType?.[2]) {
-        saveText = true
-        image = lookupFile(
-            lookup.champ,
-            slotItem
-        ) || defaultChamp
-    }
-
-    if (!image) reject(false)
-    if (saveText) saveCacheText(`cache/${team}/${slot}${saveTxt}`, slotItem)
-    saveCacheImage(image, saveSpot)
-
-    if (slotType?.[1] == 'champ' && !slotType?.[2]) {
-        image = lookupFile(
-            lookup.banner,
-            slotItem
-        ) || defaultBanner
-        if (!!image) {
-            const saveSpot = path.resolve(srcCache, team, slot + 'banner' + savePng)
-            saveCacheImage(image, saveSpot)
-        }
-    }
-
-    resolve(true)
-})
-
-export const build = (clean: boolean = false) => new Promise<{ errors: string[] }>(async resolve => {
-    let errors: string[] = []
-
-    const saveMatchName = path.resolve(srcCache, 'match' + saveTxt)
-    if (!fs.existsSync(saveMatchName)) {
-        saveCacheText(saveMatchName, 'BLUEvsRED')
-    }
-
-    for (const [team, teamData] of Object.entries(teams)) {
-        const saveSpotName = path.resolve(srcCache, team, 'name' + saveTxt)
-        if (!fs.existsSync(saveSpotName)) {
-            saveCacheText(saveSpotName, team.toUpperCase())
+export const push = (team: string, slot: string, slotItem: string) => {
+    return new Promise<boolean>((resolve, reject) => {
+        if (!(team in teams)) {
+            reject(false)
+        } else if (!(slot in teams[team])) {
+            reject(false)
         }
 
-        for (const [spot, spotItem] of Object.entries(teamData)) {
-            const item = <string>(!clean ? spotItem : '')
-            push(team, spot, item).catch(err => {
-                console.error(err)
-            })
+        teams[team][slot] = slotItem
+
+        const slotType: string[] | null = slot.match(/^(ban|champ)[1-5](spell)?[1-2]?/)
+        const saveSpot: string = path.resolve(srcCache, team, slot + savePng)
+
+        let lookup = {
+            champ: path.resolve('' + env('CHAMP_ICON_LOOKUP')),
+            banner: path.resolve('' + env('CHAMP_BANNER_LOOKUP')),
+            spell: path.resolve('' + ('SPELL_ICON_LOOKUP'))
         }
-    }
 
-    await save(errors.length === 0)
-    resolve({ errors: errors })
-})
+        let saveText: boolean = false
+        let image: string = ''
 
-export const save = (is: boolean = true) => new Promise<boolean>((resolve, reject) => {
-    if (!is) resolve(false)
+        if (slotType?.[1] == 'champ' && slotType?.[2] == 'spell') {
+            image = lookupFile(
+                lookup.spell,
+                slotItem
+            ) || defaultSpell
+        } else if (!slotType?.[2]) {
+            saveText = true
+            image = lookupFile(
+                lookup.champ,
+                slotItem
+            ) || defaultChamp
+        }
 
-    fs.writeFile(saveSrc, JSON.stringify(teams), err => {
-        if (err) reject(err)
+        if (!image) reject(false)
+        if (saveText) saveCacheText(`cache/${team}/${slot}${saveTxt}`, slotItem)
+        saveCacheImage(image, saveSpot)
+
+        if (slotType?.[1] == 'champ' && !slotType?.[2]) {
+            image = lookupFile(
+                lookup.banner,
+                slotItem
+            ) || defaultBanner
+            if (!!image) {
+                const saveSpot = path.resolve(srcCache, team, slot + 'banner' + savePng)
+                saveCacheImage(image, saveSpot)
+            }
+        }
+
         resolve(true)
     })
-})
+}
+
+export const build = (clean: boolean = false) => {
+    return new Promise<{ errors: string[] }>(async resolve => {
+        let errors: string[] = []
+
+        const saveMatchName = path.resolve(srcCache, 'match' + saveTxt)
+        if (!fs.existsSync(saveMatchName)) {
+            saveCacheText(saveMatchName, 'BLUEvsRED')
+        }
+
+        for (const [team, teamData] of Object.entries(teams)) {
+            const saveSpotName = path.resolve(srcCache, team, 'name' + saveTxt)
+            if (!fs.existsSync(saveSpotName)) {
+                saveCacheText(saveSpotName, team.toUpperCase())
+            }
+
+            for (const [spot, spotItem] of Object.entries(teamData)) {
+                const item = <string>(!clean ? spotItem : '')
+                push(team, spot, item).catch(err => {
+                    console.error(err)
+                })
+            }
+        }
+
+        await save(errors.length === 0)
+        resolve({ errors: errors })
+    })
+}
+
+export const save = (is: boolean = true) => {
+    return new Promise<boolean>((resolve, reject) => {
+        if (!is) resolve(false)
+
+        fs.writeFile(saveSrc, JSON.stringify(teams), err => {
+            if (err) reject(err)
+            resolve(true)
+        })
+    })
+}
 
 export const getState = () => {
     return teams
 }
 
 export const getRaw = () => {
-    return teams
+    return teamsStructure
 }
